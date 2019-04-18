b1934d7cae3dcfb568b78864eb1f4ae3adc5fbfa
lengai <371522155@qq.com>
2019-04-18 10:39:41 +0800

    refact spider command

--- commands.py
+++ commands.py
@@ -2,25 +2,10 @@ import click
 from requests_html import HTMLSession
 from flask.cli import with_appcontext
 from app.extensions import db
+from app.functions import spider_image
 
 
 @click.command()
 @with_appcontext
 def spider():
-    from app.servives import image_srv
-
-    session = HTMLSession()
-    page = 1
-    while True:
-        url = 'https://www.apptu.cn/wp-admin/admin-ajax.php?action=getpost&paged=' + str(page)
-        response = session.get(url)
-        datas = response.json()
-
-        if not datas:
-            return True
-        for data in datas:
-            image_srv.save(url=data.get('message'), img_id=data.get('id'), bookmarked=data.get('bookmarked'))
-            db.session.commit()
-
-        page = page + 1
-        print(url)
+    spider_image()
